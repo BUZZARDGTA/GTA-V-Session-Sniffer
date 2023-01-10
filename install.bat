92593d590846e33c98573fda06d92ce73c24f9af
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-01-10 17:28:38 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,24 +1,27 @@
 @echo off
 
+>nul 2>&1 where "pip.exe" || (
+    echo Impossible to install requirements because "pip.exe" was not found in your system.
+    echo Please check the "Add Python 3.x to PATH" checkbox on the Python setup installer, and try again.
+    echo:
+    echo Press {ANY KEY} to continue ...
+    >nul pause
+    exit /b 0
+)
+
 if exist "requirements.txt" (
-    pip install -r requirements.txt
-) else (
-    >nul 2>&1 where "pip.exe" || (
-        echo Impossible to install requirements because "pip.exe" was not found in your system.
-        echo Please check the "Add Python 3.x to PATH" checkbox on the Python setup installer, and try again.
-        echo:
-        echo Press {ANY KEY} to continue ...
-        >nul pause
-        exit 0
+    pip install -r "requirements.txt" && (
+        exit /b 0
     )
-    for %%A in (
-        pyshark
-        urllib3
-        requests
-        colorama
-    ) do (
-        pip install %%A
-    )
+)
+
+for %%A in (
+    pyshark
+    urllib3
+    requests
+    colorama
+) do (
+    pip install %%A
 )
 
 exit /b 0
